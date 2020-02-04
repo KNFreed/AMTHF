0251b6b2b5dc9acadcff42e75c044462f0f30e2c
Frédéric Spataro <44784500+KNFreed@users.noreply.github.com>
2020-02-04 01:13:35 +0100

    well

--- Emission/Script.py
+++ Emission/Script.py
@@ -5,28 +5,75 @@ from scipy.io.wavfile import read, write
 import numpy as np
 import matplotlib.pyplot as plt
 import wave, argparse, sys, time
+import os
 
-#IM GETTING TIRED OF THIS SHIT
+def fileparse():
+    # Parse des fichiers locaux à compresser
+    files = os.listdir(os.curdir)
+    return files
+
+def readfile(evt):
+    texts = []
+    t=0
+    while (t < len(evt)):
+        # Recherche des fichiers textes
+        if evt[t].endswith(".txt"):
+            f = open(evt[t], "r")
+            #Sauvegarde de leur contenus
+            texts.append(f.read())
+            f.close()
+            t=t+1
+        else:
+            t=t+1
+    return texts
+
+def compress():
+    uncompressed = readfile(fileparse())
+    compressed = []
+    #Compression de chaque texte
+    for b in range(0,len(uncompressed)):
+        #Création du dictionnaire
+
+        dict_size = 256
+        dictionary = {chr(i): i for i in range(dict_size)}
+        w = ""
+        result = []
+        for c in uncompressed[b]:
+            wc = w + c
+            if wc in dictionary:
+                w = wc
+            else:
+                compressed.append(dictionary[w])
+                # Add wc to the dictionary.
+                dictionary[wc] = dict_size
+                dict_size += 1
+                w = c
+        # Output the code for w.
+        if w:
+            compressed.append(dictionary[w])
+    return compressed
+
+def dectobin(evt):
+    binary=[]
+    for j in range (0,len(evt)):
+        binary = binary+[int(x) for x in list('{0:0b}'.format(evt[j]))]
+    print(evt)
+    print(binary)
+    return binary
 
 #shit to transfer
-chain = [0,1,0,1]
-
-#shitty amplitudes for the ask
-AH = 1
-AB = 0.5
-
-#samplingrate cunt
-samplingrate = 22000
+chains = dectobin(compress())
 
 N = 44100
 tiv = 1/N
 t=0
 r=0
 signal = []
-print(len(chain))
 
-while(t<len(chain)):
-    signal=signal+[chain[int(t)]*np.sin(2*np.pi*21000*t)]
+
+
+while(t<len(chains)):
+    signal=signal+[chains[int(t)]*np.sin(2*np.pi*21000*t)]
     t = t+tiv
     print(t)
 print(signal)
